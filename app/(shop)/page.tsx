148306f024687bc0c9179c73314105600cae89b8
Jonathan Christian Mendoza Ramos <patts2810@gmail.com>
2023-12-29 19:43:11 -0500

    feat: create product grid

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,4 +1,7 @@
-import { Title } from '@/components'
+import { ProductGrid, Title } from '@/components'
+import { initialData } from '@/seed/seed'
+
+const products = initialData.products
 
 export default function Home() {
   return (
@@ -8,6 +11,8 @@ export default function Home() {
         subtitle='All products'
         className='mb-2'
       />
+
+      <ProductGrid products={products} />
     </>
   )
 }
