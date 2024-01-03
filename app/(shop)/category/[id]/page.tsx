60331c2a6c9914b66039ef50f5a01646b39e3e47
Jonathan Christian Mendoza Ramos <patts2810@gmail.com>
2024-01-02 19:40:05 -0500

    feat: display categories screens

--- app/(shop)/category/[id]/page.tsx
+++ app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,43 @@
+import { ProductGrid, Title } from '@/components'
+import { initialData } from '@/seed/seed'
 import { notFound } from 'next/navigation'
+import { Product, Category } from '@/interfaces'
 
 interface Props {
   params: {
-    id: string
+    id: Category
   }
 }
+
+const products = initialData.products
 
 export default function Page({ params }: Props) {
   const { id } = params
 
-  if (id === 'kids') {
-    notFound()
+  // if (id !== Category ) {
+  //   notFound()
+  // }
+
+  const labels: Record<Category, string> = {
+    'men': 'for men',
+    'women': 'for women',
+    'kid': 'for kids',
+    'unisex': 'for everyone'
   }
+
+  console.log(id)
+
+  const filteredProducts = products.filter((product) => product.gender === id)
   
 
   return (
     <div>
-      <h1>Category Page {id}</h1>
+      <Title
+        title={`Articles ${labels[id]}`}
+        subtitle={`Category: ${id}`}
+        className='mb-2'
+      />
+      <ProductGrid products={filteredProducts} />
     </div>
   )
 }
